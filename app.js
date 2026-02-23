5c57cf6ef8355419203b28e9e45308c026fb8c32
glenfordmcfarlane-ux <glenford.mcfarlane@gmail.com>
2026-02-23 12:07:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+  for (let i = 1; i <= config.total; i++){
+    const b = document.createElement("button");
+    b.className = "gridbtn";
+    b.type = "button";
+    b.textContent = pad2(i);
+    b.addEventListener("click", () => openCard(i));
+    grid.appendChild(b);
+  }
